fdc12d3cabcc8fd6ae73e1fb49430ca982ae576c
coreyreuter-droid <coreyreuter@gmail.com>
2025-12-04 21:11:16 +0000

    mobile

--- components/CardContainer.tsx
+++ components/CardContainer.tsx
@@ -1,8 +1,7 @@
 export default function CardContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="rounded-lg bg-white p-4 sm:p-6 shadow">
+    <div className="rounded-lg bg-white p-4 sm:p-6 shadow w-full max-w-full overflow-hidden">
       {children}
     </div>
   );
 }
-
