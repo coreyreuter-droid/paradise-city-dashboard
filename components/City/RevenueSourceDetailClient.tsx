8a4a37e986efaf23420a02c6bd4e8f334097d105
Corey Reuter <coreyreuter@gmail.com>
2026-01-18 15:51:17 -0600

    update deploy error

--- components/City/RevenueSourceDetailClient.tsx
+++ components/City/RevenueSourceDetailClient.tsx
@@ -277,7 +277,7 @@ export default function RevenueSourceDetailClient({
       </div>
 
       {/* Narrative */}
-      <NarrativeSummary text={narrative} />
+      <NarrativeSummary narrative={narrative} />
 
       {/* Summary cards */}
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
